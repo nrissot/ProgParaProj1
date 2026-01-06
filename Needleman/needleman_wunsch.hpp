6feb866bd410ac0e6a2e13d6d1fd3f72d949b239
Kaawan <kaawan.jdr@gmail.com>
2026-01-06 10:13:49 +0100

    Fix Needleman Wunsch to implement the difference between the expension (penalty -1) and the creation (penalty -3) of holes.

--- Needleman/needleman_wunsch.hpp
+++ Needleman/needleman_wunsch.hpp
@@ -7,15 +7,14 @@
 using namespace std;
 
 #define MATCH_REWARD 1
-
 #define MISMATCH_PENALTY -1
-
 #define GAP_PENALTY -3
+#define EXTEND_PENALTY -1
 
 int nw_distance_seq(const char *A, int lenA, const char *B, int lenB);
 
 int nw_distance_omp(const char *A, int lenA, const char *B, int lenB);
 
-void _choose_and_write(int* F, int i, int j, char a, char b, int lenA, int lenB);
+void _choose_and_write(int *F, bool *H, int i, int j, char a, char b, int lenA, int lenB);
 
 #endif
